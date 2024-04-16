hanzoai/pubsub

Reuse `SublistResult` for `LoadNextMsgMulti`
This should improve performance a bit by reducing the number of
`SublistResult`s that we need to allocate on each hit.

Signed-off-by: Neil Twigg <neil@nats.io>

server/filestore.go
@@ -2265,6 +2265,7 @@ func (mb *msgBlock) firstMatchingMulti(sl *Sublist, start uint64, sm *StoreMsg) 
 		sm = new(StoreMsg)
 	}
 
+	var result SublistResult
 	for seq := start; seq <= lseq; seq++ {
 		llseq := mb.llseq
 		fsm, err := mb.cacheLookup(seq, sm)
@@ -2273,7 +2274,7 @@ func (mb *msgBlock) firstMatchingMulti(sl *Sublist, start uint64, sm *StoreMsg) 
 		}
 		expireOk := seq == lseq && mb.llseq == seq
 
-		if r := sl.Match(fsm.subj); len(r.psubs) > 0 {
+		if r := sl.MatchWithResult(fsm.subj, &result); len(r.psubs) > 0 {
 			return fsm, expireOk, nil
 		}
 		// If we are here we did not match, so put the llseq back.

server/memstore.go
@@ -1055,12 +1055,13 @@ func (ms *memStore) LoadNextMsgMulti(sl *Sublist, start uint64, smp *StoreMsg) (
 	// Initial setup.
 	fseq, lseq := start, ms.state.LastSeq
 
+	var result SublistResult
 	for nseq := fseq; nseq <= lseq; nseq++ {
 		sm, ok := ms.msgs[nseq]
 		if !ok {
 			continue
 		}
-		if r := sl.Match(sm.subj); len(r.psubs) > 0 {
+		if r := sl.MatchWithResult(sm.subj, &result); len(r.psubs) > 0 {
 			if smp == nil {
 				smp = new(StoreMsg)
 			}

server/sublist.go
@@ -527,14 +527,21 @@ var emptyResult = &SublistResult{}
 // Match will match all entries to the literal subject.
 // It will return a set of results for both normal and queue subscribers.
 func (s *Sublist) Match(subject string) *SublistResult {
-	return s.match(subject, true)
+	return s.match(subject, true, &SublistResult{})
+}
+
+// MatchWithResult will match all entries to the literal subject, reusing the
+// supplied SublistResult to reduce allocations on hot paths.
+// It will return a set of results for both normal and queue subscribers.
+func (s *Sublist) MatchWithResult(subject string, result *SublistResult) *SublistResult {
+	return s.match(subject, true, result)
 }
 
 func (s *Sublist) matchNoLock(subject string) *SublistResult {
-	return s.match(subject, false)
+	return s.match(subject, false, &SublistResult{})
 }
 
-func (s *Sublist) match(subject string, doLock bool) *SublistResult {
+func (s *Sublist) match(subject string, doLock bool, result *SublistResult) *SublistResult {
 	atomic.AddUint64(&s.matches, 1)
 
 	// Check cache first.
@@ -569,7 +576,8 @@ func (s *Sublist) match(subject string, doLock bool) *SublistResult {
 	tokens = append(tokens, subject[start:])
 
 	// FIXME(dlc) - Make shared pool between sublist and client readLoop?
-	result := &SublistResult{}
+	result.psubs = result.psubs[:0]
+	result.qsubs = result.qsubs[:0]
 
 	// Get result from the main structure and place into the shared cache.
 	// Hold the read lock to avoid race between match and store.

ReubenMathew/nats-server

server/filestore.go
@@ -2265,6 +2265,7 @@ func (mb *msgBlock) firstMatchingMulti(sl *Sublist, start uint64, sm *StoreMsg) 
 		sm = new(StoreMsg)
 	}
 
+	var result SublistResult
 	for seq := start; seq <= lseq; seq++ {
 		llseq := mb.llseq
 		fsm, err := mb.cacheLookup(seq, sm)
@@ -2273,7 +2274,7 @@ func (mb *msgBlock) firstMatchingMulti(sl *Sublist, start uint64, sm *StoreMsg) 
 		}
 		expireOk := seq == lseq && mb.llseq == seq
 
-		if r := sl.Match(fsm.subj); len(r.psubs) > 0 {
+		if r := sl.MatchInline(fsm.subj, &result); len(r.psubs) > 0 {
 			return fsm, expireOk, nil
 		}
 		// If we are here we did not match, so put the llseq back.

server/memstore.go
@@ -1055,12 +1055,13 @@ func (ms *memStore) LoadNextMsgMulti(sl *Sublist, start uint64, smp *StoreMsg) (
 	// Initial setup.
 	fseq, lseq := start, ms.state.LastSeq
 
+	var result SublistResult
 	for nseq := fseq; nseq <= lseq; nseq++ {
 		sm, ok := ms.msgs[nseq]
 		if !ok {
 			continue
 		}
-		if r := sl.Match(sm.subj); len(r.psubs) > 0 {
+		if r := sl.MatchInline(sm.subj, &result); len(r.psubs) > 0 {
 			if smp == nil {
 				smp = new(StoreMsg)
 			}

server/sublist.go
@@ -527,14 +527,21 @@ var emptyResult = &SublistResult{}
 // Match will match all entries to the literal subject.
 // It will return a set of results for both normal and queue subscribers.
 func (s *Sublist) Match(subject string) *SublistResult {
-	return s.match(subject, true)
+	return s.match(subject, true, &SublistResult{})
+}
+
+// MatchInline will match all entries to the literal subject, reusing the
+// supplied SublistResult to reduce allocations on hot paths.
+// It will return a set of results for both normal and queue subscribers.
+func (s *Sublist) MatchInline(subject string, result *SublistResult) *SublistResult {
+	return s.match(subject, true, result)
 }
 
 func (s *Sublist) matchNoLock(subject string) *SublistResult {
-	return s.match(subject, false)
+	return s.match(subject, false, &SublistResult{})
 }
 
-func (s *Sublist) match(subject string, doLock bool) *SublistResult {
+func (s *Sublist) match(subject string, doLock bool, result *SublistResult) *SublistResult {
 	atomic.AddUint64(&s.matches, 1)
 
 	// Check cache first.
@@ -569,7 +576,8 @@ func (s *Sublist) match(subject string, doLock bool) *SublistResult {
 	tokens = append(tokens, subject[start:])
 
 	// FIXME(dlc) - Make shared pool between sublist and client readLoop?
-	result := &SublistResult{}
+	result.psubs = result.psubs[:0]
+	result.qsubs = result.qsubs[:0]
 
 	// Get result from the main structure and place into the shared cache.
 	// Hold the read lock to avoid race between match and store.